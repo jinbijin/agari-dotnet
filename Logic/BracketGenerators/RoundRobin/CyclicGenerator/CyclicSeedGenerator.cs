5f87a5a3763a9f9d57b4f0b67cdbb8cb110839f5
Jinbi Jin <jinbijin@jinbijin.nl>
2020-01-18 23:35:25 +0100

    Define CyclicSeedGenerator

---
+++ Logic/BracketGenerators/RoundRobin/CyclicGenerator/CyclicSeedGenerator.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Logic.BracketGenerators.RoundRobin.CyclicGenerator
+{
+    public class CyclicSeedGenerator : ICyclicSeedGenerator
+    {
+        public IEnumerable<(int, int)> GenerateSeed(int roundCount, int participantCount)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Logic/BracketGenerators/RoundRobin/CyclicGenerator/ICyclicSeedGenerator.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Logic.BracketGenerators.RoundRobin.CyclicGenerator
+{
+    public interface ICyclicSeedGenerator
+    {
+        IEnumerable<(int, int)> GenerateSeed(int roundCount, int participantCount);
+    }
+}
