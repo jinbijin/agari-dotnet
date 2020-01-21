2e1a72d96045dd7e9a0e90cbbab1858da50388ce
Jinbi Jin <jinbijin@jinbijin.nl>
2020-01-22 00:19:32 +0100

    Implement CyclicGenerator

--- Logic/BracketGenerators/RoundRobin/Cyclic/CyclicGenerator.cs
+++ Logic/BracketGenerators/RoundRobin/Cyclic/CyclicGenerator.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Logic.Types.Bracket;
 using Logic.Types.Exceptions;
 
@@ -12,11 +14,59 @@ namespace Logic.BracketGenerators.RoundRobin.Cyclic
             _cyclicSeedGenerator = cyclicSeedGenerator;
         }
 
+        private const string NotSupportedRoundCountMessage = "Number of rounds must be at least 4.";
+        private const string NotSupportedParticipantCountMessage = "Number of participants should be a multiple of 4.";
+
         /// <exception cref="InvalidParameterException"/>
         /// <exception cref="SeedNotFoundException"/>
-        public Task<Bracket> GenerateBracket(int roundCount, int participantCount)
+        public async Task<Bracket> GenerateBracket(int roundCount, int participantCount)
         {
-            throw new System.NotImplementedException();
+            ValidateRoundCount(roundCount);
+            ValidateParticipantCount(participantCount);
+
+            IEnumerable<List<int>> seed = await _cyclicSeedGenerator.GenerateSeed(roundCount, participantCount / 4);
+
+            IEnumerable<BracketRound> rounds = seed.Select((p, i) => RoundFromSeed(p, participantCount / 4, i));
+
+            return new Bracket
+            {
+                Rounds = rounds.ToList()
+            };
+        }
+
+        private static BracketRound RoundFromSeed(List<int> seed, int modulus, int shift) =>
+            new BracketRound
+            {
+                Games = Enumerable.Range(1, modulus).Select(j => new BracketGame
+                {
+                    ParticipantNrs = Enumerable.Range(0, 4)
+                        .Select(x => Enumerable.Range(0, x).Sum(y => seed[y % 4]))
+                        .Select(x => (x + j + shift) % modulus)
+                        .Zip(Enumerable.Range(1, 4))
+                        .Select(CoordinatesToNumber)
+                        .OrderBy(x => x)
+                        .ToList()
+                }).ToList()
+            };
+
+        private static int CoordinatesToNumber((int, int) coordinates) => coordinates.Item1 * 4 + coordinates.Item2;
+
+        /// <exception cref="InvalidParameterException"/>
+        private void ValidateRoundCount(int roundCount)
+        {
+            if (roundCount < 4)
+            {
+                throw new InvalidParameterException(NotSupportedRoundCountMessage);
+            }
+        }
+
+        /// <exception cref="InvalidParameterException"/>
+        private void ValidateParticipantCount(int participantCount)
+        {
+            if (participantCount % 4 != 0)
+            {
+                throw new InvalidParameterException(NotSupportedParticipantCountMessage);
+            }
         }
     }
 }
