5047bf1b3d6001f56d1371cb4a60fa4491a9c2a4
Jinbi Jin <jinbijin@jinbijin.nl>
2020-01-25 01:45:36 +0100

    Attempt fix 3

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,4 +1,4 @@
 dotnet publish -c Release --runtime ubuntu.18.04-x64 -o /tmp/AgariWebApi
 ssh jinbijin@84.22.102.178 'pkill -x WebApi'
-rsync -r --delete-after --quiet /tmp/AgariWebApi jinbijin@84.22.102.178:~/AgariWebApi
+rsync -r --delete-after --quiet /tmp/AgariWebApi/ jinbijin@84.22.102.178:~/AgariWebApi
 ssh jinbijin@84.22.102.178 './AgariWebApi/WebApi &'
