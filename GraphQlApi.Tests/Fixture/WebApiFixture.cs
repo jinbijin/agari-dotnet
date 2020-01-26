9fc9c210c6b5d9c8ee3c1a1531f4c84dfbf8f02c
Jinbi Jin <jinbijin@jinbijin.nl>
2020-01-26 19:25:35 +0100

    Refactor end-to-end tests

---
+++ GraphQlApi.Tests/Fixture/WebApiFixture.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net.Http;
+using Autofac.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Hosting;
+using WebApi;
+
+namespace GraphQlApi.Tests.Fixture
+{
+    public class WebApiFixture : IDisposable
+    {
+        public HttpClient Client { get; private set; }
+
+        public WebApiFixture()
+        {
+            Environment.SetEnvironmentVariable("SNAPSHOOTER_STRICT_MODE", "on");
+            IHostBuilder hostBuilder = Host.CreateDefaultBuilder()
+                .UseServiceProviderFactory(new AutofacServiceProviderFactory())
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseTestServer();
+                    webBuilder.UseStartup<Startup>();
+                });
+            Client = hostBuilder.Start().GetTestServer().CreateClient();
+        }
+
+        public void Dispose()
+        {
+            Client.Dispose();
+        }
+    }
+}
---
+++ GraphQlApi.Tests/Variables/GenerateBracketVariables.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace GraphQlApi.Tests.Variables
+{
+    public class GenerateBracketVariables
+    {
+        [SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "JSON property")]
+        public int roundCount { get; set; }
+        [SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "JSON property")]
+        public int participantCount { get; set; }
+    }
+}
