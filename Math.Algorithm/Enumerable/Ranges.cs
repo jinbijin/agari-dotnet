5b3b6aeddf3d9663410bbd7c3af4996610b48bf2
Jinbi Jin <jinbijin@jinbijin.nl>
2021-07-08 21:14:38 +0000

    Add docstring

--- Math.Algorithm/Enumerable/Ranges.cs
+++ Math.Algorithm/Enumerable/Ranges.cs
@@ -4,6 +4,10 @@ namespace Math.Algorithm.Enumerable
 {
     public static class Ranges
     {
+        /// <summary>
+        /// Returns the range from <paramref name="start"/> up to (and including)
+        /// the largest integer n such that <paramref name="powerBase"/> raised to the power n is at most <paramref name="max"/>.
+        /// </summary>
         public static IEnumerable<int> FromUntilLog(int start, int powerBase, int max) => new RangeFromUntilLogEnumerable(start, powerBase, max);
     }
 }
