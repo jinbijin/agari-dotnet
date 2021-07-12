6cd96468ddcb1c186cd7802363d96b5304c65a23
Jinbi Jin <jinbijin@jinbijin.nl>
2021-07-12 21:25:05 +0000

    Add odd prime field class

---
+++ Math.Field/OddPrimeField.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Math.Field
+{
+    public class OddPrimeField
+    {
+        private readonly int _prime;
+        private List<OddPrimeFieldElement>? _elements;
+        private OddPrimeFieldElement? _primitiveRoot;
+        private List<OddPrimeFieldElement>? _units;
+
+        public int Characteristic => _prime;
+        public int Size => _prime;
+        public IReadOnlyCollection<OddPrimeFieldElement> Elements => _elements ?? ComputeElements();
+        public OddPrimeFieldElement PrimitiveRoot => _primitiveRoot ?? ComputePrimitiveRoot();
+        public IReadOnlyCollection<OddPrimeFieldElement> Units => _units ?? ComputeUnits();
+
+        public OddPrimeField(int prime)
+        {
+            _prime = prime;
+        }
+
+        public OddPrimeFieldElement Create(int value) => new() { Parent = this, Value = value };
+
+        private List<OddPrimeFieldElement> ComputeElements()
+        {
+            List<OddPrimeFieldElement> result = Enumerable.Range(0, _prime).Select(value => new OddPrimeFieldElement { Parent = this, Value = value }).ToList().Shuffle();
+            _elements = result;
+            return result;
+        }
+
+        private OddPrimeFieldElement ComputePrimitiveRoot()
+        {
+            foreach (OddPrimeFieldElement element in Elements.Where(element => element.HasMultiplicativeInverse))
+            {
+                if (IsPrimitiveRoot(element))
+                {
+                    _primitiveRoot = element;
+                    return element;
+                }
+            }
+
+            throw new InvalidOperationException("Unreachable");
+        }
+
+        private bool IsPrimitiveRoot(OddPrimeFieldElement element)
+        {
+            OddPrimeFieldElement power = Create(1);
+            foreach (int exponent in Enumerable.Range(1, _prime / 2))
+            {
+                power *= element;
+                if (power.IsMultiplicativeUnit)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private List<OddPrimeFieldElement> ComputeUnits()
+        {
+            List<OddPrimeFieldElement> result = new();
+            OddPrimeFieldElement power = Create(1);
+            foreach (int exponent in Enumerable.Range(1, _prime - 1))
+            {
+                power *= PrimitiveRoot;
+                result.Add(power);
+            }
+            _units = result;
+            return result;
+        }
+    }
+}
---
+++ Math.Polynomial/Polynomial.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.Linq;
+
+namespace Math.Polynomial
+{
+    public readonly struct Polynomial
+    {
+        public int Degree => Coefficients.Length - 1;
+
+        /// <summary>
+        /// The array of coefficients, from low degree to high degree.
+        /// </summary>
+        public readonly ImmutableArray<int> Coefficients { get; init; }
+
+        public static Polynomial operator *(in Polynomial first, in Polynomial second)
+        {
+            int degree = first.Degree + second.Degree;
+            var coefficientArray = new int[degree + 1];
+
+            for (int i = 0; i <= first.Degree; i++)
+            {
+                for (int j = 0; j <= first.Degree; j++)
+                {
+                    coefficientArray[i + j] += first.Coefficients[i] * second.Coefficients[j];
+                }
+            }
+
+            return new() { Coefficients = coefficientArray.ToImmutableArray() };
+        }
+
+        public static Polynomial CreateIrreducibleMod(int degree, int prime)
+        {
+            if (prime < 2) { throw new ArgumentOutOfRangeException(nameof(prime), "Modulus must be at least 2."); }
+            if (degree < 2) { throw new ArgumentOutOfRangeException(nameof(degree), "Degree must be at least 2."); }
+
+            if (degree > 2) { throw new NotImplementedException(); }
+
+            foreach (var (c0, c1) in Enumerable.Range(0, prime).SelectMany(c0 => Enumerable.Range(0, prime).Select(c1 => (c0, c1))))
+            {
+                var candidate = new Polynomial { Coefficients = new[] { c0, c1, 1 }.ToImmutableArray() };
+                if (Enumerable.Range(0, prime).All(d => ((d*(c1 - d) - c0) % prime) != 0))
+                {
+                    return candidate;
+                }
+            }
+
+            throw new InvalidOperationException("No result found.");
+        }
+    }
+}
