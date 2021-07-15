18edb62e537e6f52700f2842fa711626e58a5d84
Jinbi Jin <jinbijin@jinbijin.nl>
2021-07-15 19:36:31 +0000

    Add randomness to finite field generator

---
+++ Logic.Common/Random/RandomEnumerableExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Logic.Common.Random
+{
+    public static class RandomEnumerableExtensions
+    {
+        private static System.Random _random = new System.Random();
+
+        public static IList<T> Shuffle<T>(this IEnumerable<T> source)
+        {
+            List<T> list = new();
+            foreach (var (item, index) in source.Select((item, index) => (item, index)))
+            {
+                var insertIndex = _random.Next(index);
+                list.Insert(insertIndex, item);
+            }
+
+            return list;
+        }
+    }
+}
