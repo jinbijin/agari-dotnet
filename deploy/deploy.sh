c90cc512d81a844ba1adb55a91ed2b47ab23d434
jinbijin <35583057+jinbijin@users.noreply.github.com>
2023-04-18 20:58:43 +0200

    Fix release script

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,4 +1,4 @@
-dotnet publish WebApi/WebApi.csproj -c Release --runtime ubuntu.18.04-x64 -o /tmp/AgariWebApi
+dotnet publish WebApi/WebApi.csproj -c Release --os linux -o /tmp/AgariWebApi --self-contained
 ssh jinbijin@84.22.102.178 'pkill -x WebApi'
 rsync -r --delete-after --quiet /tmp/AgariWebApi/ jinbijin@84.22.102.178:~/AgariWebApi
 ssh -f jinbijin@84.22.102.178 './AgariWebApi/WebApi'
